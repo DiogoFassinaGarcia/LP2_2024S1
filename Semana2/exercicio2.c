b82c19a26b2a9c9a8d25a913f2c782c87a0dbdc9
diogo garcia <160797380+madlagoon@users.noreply.github.com>
2024-03-08 16:16:47 -0300

    Update exercicio2.c

--- Semana2/exercicio2.c
+++ Semana2/exercicio2.c
@@ -7,5 +7,5 @@ int main(void) {
   printf("insira sua altura: ");
   scanf("%f", &altu);
   printf("O IMC de uma pessoa com peso %f kg e altura %f m é igual a %f.", peso, altu, peso / (altu * altu));
-return 0
+return 0;
 }
