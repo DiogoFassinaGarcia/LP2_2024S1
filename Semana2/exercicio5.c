6f574571d6cc14056383865b07777cdee34cecce
diogo garcia <160797380+madlagoon@users.noreply.github.com>
2024-03-08 16:14:17 -0300

    exercicio5.c

---
+++ Semana2/exercicio5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+float raio;
+int main(void){
+  printf("escreva o raio do círculo: ");
+  scanf("%f", &raio);
+  printf("Um círculo com raio %f tem área igual a %f.", raio, M_PI * (raio * raio));
+}
