90426f2b5183348b07d7a06fddf88c2816d12598
diogo garcia <160797380+madlagoon@users.noreply.github.com>
2024-03-08 16:07:12 -0300

    exercicio1.c

---
+++ Semana2/exercicio1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int num1, num2;
+int main(void) {
+  printf("coloque dois números: ");
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+  printf("%d, %d, %d, %d", num1 + num2, num1 - num2, num1 * num2, num1 / num2);
