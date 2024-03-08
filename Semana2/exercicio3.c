a1d1b805e4bbed4ff02b7e8a55429eaa37543e82
diogo garcia <160797380+madlagoon@users.noreply.github.com>
2024-03-08 16:16:33 -0300

    Update exercicio3.c

--- Semana2/exercicio3.c
+++ Semana2/exercicio3.c
@@ -4,4 +4,5 @@ int main(void){
   printf("insira a temperatura em celsius: ");
   scanf("%f", &celsius);
   printf("Uma temperatura de %f graus Celsius equivale a %f graus Fahrenheit", celsius , ((celsius * 9 / 5) + 32));
+return 0;
 }
