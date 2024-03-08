5beac3d5ef6e7b2df26cb7f75e99fb153d435e39
diogo garcia <160797380+madlagoon@users.noreply.github.com>
2024-03-08 16:09:19 -0300

    Update exercicio2.c

--- Semana2/exercicio2.c
+++ Semana2/exercicio2.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
 float altu, peso;
+
+int main(void) {
+  printf("insira seu peso: ");
+  scanf("%f", &peso);
+  printf("insira sua altura: ");
+  scanf("%f", &altu);
+  printf("O IMC de uma pessoa com peso %f kg e altura %f m é igual a %f.", peso, altu, peso / (altu * altu));
+}
