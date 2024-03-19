1176c074784e49fff90896a3b876f94fbc9c7d5b
diogo garcia <160797380+DiogoFassinaGarcia@users.noreply.github.com>
2024-03-18 21:51:45 -0300

    Update exercicio5.c

--- Semana4/exercicio5.c
+++ Semana4/exercicio5.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <math.h>
 
+float funcao(int n){
+  float resposta = 0;
+  float aux;
+  for (int i = 1; i < n + 1; i++){
+    aux = ((float)i / pow(i, 2)) * pow(-1, (float)i + 1);
+    resposta = resposta + aux;
+  }
+  return resposta;
+}
+
+int x = 0;
+
+int main(void) {
+  printf("insira o valor de n: ");
+  scanf("%d", &x);
+
+  printf("o valor da função para o n dado é aproximadamente: %f", funcao(x));
+
+  return 0;
+}
